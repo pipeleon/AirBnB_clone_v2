67044bdeb66bd161a2e9e6c8fc8eddd7f6d08306
pipeleon <oflg94@gmail.com>
2022-05-14 19:55:21 -0700

    Files to configurate mysql for this project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- script that prepares the db hbnb_dev_db in mysql and a user hbnb_dev to manage it
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
